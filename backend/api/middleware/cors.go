4ff0387e87c554cff779c51461f1bfb02e6cb2d7
Kyle Boehlen <kyleboehlen@gmail.com>
2025-10-09 14:20:03 -0600

    Removing open origins from CORS

--- backend/api/middleware/cors.go
+++ backend/api/middleware/cors.go
@@ -1,10 +1,20 @@
 package middleware
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+)
 
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
+		var origin = "https://portraitpear.photography"
+
+		var debug, exists = os.LookupEnv("DEBUG_CORS")
+		if exists && debug == "true" {
+			origin = "http://localhost:5173"
+		}
+
+		w.Header().Set("Access-Control-Allow-Origin", origin)
 		w.Header().Set("Vary", "Origin")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Methods", "GET,POST,DELETE")
